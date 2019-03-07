9e8ad4f222eb169c27106241a447f09468c58979
stev0531 <stev0531@morris.umn.edu>
2019-03-06 18:00:43 -0600

    Finishing up exercises.

--- express.js
+++ express.js
@@ -1,9 +1,11 @@
 var express = require('express');
 app = express(),
-    port = process.env.PORT || 1337;
-
-//var app = angular.module('MyTutorialApp',[]);
+port = process.env.PORT || 1337;
 
 app.use(express.static(__dirname + '/public'));
+app.get("/buttons",function(req,res){
+    res.send("Hello World! May I interest you in some... <em>buttons</em>?")
+});
+
 app.listen(port);
 
--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -1,12 +1,43 @@
+Promise = require('bluebird');
+mysql = require('mysql');
+DBF = require('../dbf-setup.js');
+
 var express=require('express'),
 app = express(),
 port = process.env.PORT || 1337;
 
-var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
+var buttons= function(){ //Returns a promise that can take a handler ready to process the results.
+    var sql = "select * from forever_alone.till_buttons;";
+    var result = DBF.query(mysql.format(sql));
+
+    result.then(function(dbfs,err){
+        var arr = new Array(dbfs.length);
+        dbfs.forEach(function (item, index) {
+
+            arr[index] = {
+                "buttonID": item.buttonID,
+                "left": item.left_pos,
+                "top": item.top,
+                "width": item.width,
+                "label": item.label,
+                "invID": item.invID
+            };
+        })
+        return arr;
+    })
+
+    //return arr;
+
+};
 
 app.use(express.static(__dirname + '/public')); //Serves the web pages
 app.get("/buttons",function(req,res){ // handles the /buttons API
-  res.send(buttons);
+
+    // buttons().then(function (bts) {
+    //     res.send(bts)
+    // });
+    res.send(buttons())
+
 });
 
 app.listen(port);
