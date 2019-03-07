a2f61e7dd4826cf1e84404e81689dc33e2a8ecba
hassa357 <hassa357@morris.umn.edu>
2019-03-07 13:55:52 -0600

    Finish Lab. The query returns the data in the way we want it to return.

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -6,38 +6,38 @@ var express=require('express'),
 app = express(),
 port = process.env.PORT || 1337;
 
-var buttons= function(){ //Returns a promise that can take a handler ready to process the results.
-    var sql = "select * from forever_alone.till_buttons;";
-    var result = DBF.query(mysql.format(sql));
+//var buttons= function(){ //Returns a promise that can take a handler ready to process the results.
+var sql = "select * from forever_alone.till_buttons;";
+var result = DBF.query(mysql.format(sql));
 
-    result.then(function(dbfs,err){
-        var arr = new Array(dbfs.length);
-        dbfs.forEach(function (item, index) {
 
-            arr[index] = {
-                "buttonID": item.buttonID,
-                "left": item.left_pos,
-                "top": item.top,
-                "width": item.width,
-                "label": item.label,
-                "invID": item.invID
+
+result.then(function(dbfs,err){
+
+    // Creates an array that holds the data
+
+    var bts = new Array(dbfs.length);
+    dbfs.forEach(function (item, index) {
+
+        // Returns the results of the query in an array with the let_pos as left as "left" is a command in the DB.
+
+        bts[index] = {
+            "buttonID": item.buttonID,
+            "left": item.left_pos,
+            "top": item.top,
+            "width": item.width,
+            "label": item.label,
+            "invID": item.invID
             };
         })
-        return arr;
-    })
-
-    //return arr;
-
-};
 
 app.use(express.static(__dirname + '/public')); //Serves the web pages
 app.get("/buttons",function(req,res){ // handles the /buttons API
-
-    // buttons().then(function (bts) {
-    //     res.send(bts)
-    // });
-    res.send(buttons())
-
+    res.send(bts)
 });
 
 app.listen(port);
+
+});
+
+
